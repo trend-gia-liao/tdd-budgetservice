adc48cdadafe8eb886bd4329d4b3e46395ec4b5d
gia_liao <gia_liao@trendmicro.com>
2024-08-21 15:41:06 +0800

    init

---
+++ budget_service/budget_service.py
@@ -0,0 +1,11 @@
+class BudgetSevice:
+    def totalAmount(self, start, end):
+        return 0
+
+class Budget:
+    YearMonth: str
+    Amount: int
+
+class BudgetRepo:
+    def getAll(self):
+        return []
