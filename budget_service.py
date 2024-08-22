83f83abe675387443470833313c389a5a33d0ffd
gia_liao <gia_liao@trendmicro.com>
2024-08-22 17:47:37 +0800

    rename variable: total_amount

--- budget_service.py
+++ budget_service.py
@@ -28,17 +28,17 @@ class BudgetService:
         if start.year == end.year and start.month == end.month:
             return get_single_day_amount(start.year, start.month) * (end.day - start.day + 1)
 
-        total = get_single_day_amount(start.year, start.month) * (
+        total_amount = get_single_day_amount(start.year, start.month) * (
                 get_days_in_month(start.year, start.month) - start.day + 1) + get_single_day_amount(end.year,
                                                                                                     end.month) * end.day
         current_date = start + relativedelta(months=1)
         end_month = datetime.date(end.year, end.month, 1)
         while current_date < end_month:
-            total += get_single_day_amount(current_date.year, current_date.month) * get_days_in_month(current_date.year,
+            total_amount += get_single_day_amount(current_date.year, current_date.month) * get_days_in_month(current_date.year,
                                                                                                       current_date.month)
             current_date += relativedelta(months=1)
 
-        return total
+        return total_amount
 
 
 class Budget:
