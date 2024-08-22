01fe8ecb620dce5cf55f3f1e593addde9b6a9ef6
gia_liao <gia_liao@trendmicro.com>
2024-08-22 17:45:39 +0800

    rename naming to snake style

--- budget_service.py
+++ budget_service.py
@@ -4,54 +4,54 @@ import datetime
 from dateutil.relativedelta import relativedelta
 
 
-def getDaysInMonth(year, month):
+def get_days_in_month(year, month):
     return calendar.monthrange(year, month)[1]
 
 
-def getSingleDayAmount(year, month):
-    startYearMonth = "{:04d}{:02d}".format(year, month)
-    records = BudgetRepo().getAll()
+def get_single_day_amount(year, month):
+    start_year_month = "{:04d}{:02d}".format(year, month)
+    records = BudgetRepo().get_all()
     for record in records:
-        if record.YearMonth == startYearMonth:
-            monthTotoldays = getDaysInMonth(year, month)
-            return record.Amount / monthTotoldays
+        if record.year_month == start_year_month:
+            month_total_days = get_days_in_month(year, month)
+            return record.amount / month_total_days
 
     return 0
 
 
 class BudgetService:
-    def totalAmount(self, start, end):
+    def get_total_amount(self, start, end):
 
         if start > end:
             return 0
 
         if start.year == end.year and start.month == end.month:
-            return getSingleDayAmount(start.year, start.month) * (end.day - start.day + 1)
+            return get_single_day_amount(start.year, start.month) * (end.day - start.day + 1)
 
-        total = getSingleDayAmount(start.year, start.month) * (
-                getDaysInMonth(start.year, start.month) - start.day + 1) + getSingleDayAmount(end.year,
-                                                                                              end.month) * end.day
+        total = get_single_day_amount(start.year, start.month) * (
+                get_days_in_month(start.year, start.month) - start.day + 1) + get_single_day_amount(end.year,
+                                                                                                    end.month) * end.day
         current_date = start + relativedelta(months=1)
         end_month = datetime.date(end.year, end.month, 1)
         while current_date < end_month:
-            total += getSingleDayAmount(current_date.year, current_date.month) * getDaysInMonth(current_date.year,
-                                                                                                current_date.month)
+            total += get_single_day_amount(current_date.year, current_date.month) * get_days_in_month(current_date.year,
+                                                                                                      current_date.month)
             current_date += relativedelta(months=1)
 
         return total
 
 
 class Budget:
-    YearMonth: str
-    Amount: int
+    year_month: str
+    amount: int
 
-    def __init__(self, YearMonth, Amount):
-        self.YearMonth = YearMonth
-        self.Amount = Amount
+    def __init__(self, year_month, amount):
+        self.year_month = year_month
+        self.amount = amount
 
 
 class BudgetRepo:
-    def getAll(self):
+    def get_all(self):
         return [
             Budget("202405", 310),
             Budget("202406", 300),
--- budget_service_test.py
+++ budget_service_test.py
@@ -7,52 +7,52 @@ from budget_service import BudgetService
 class BudgetServiceTestCase(unittest.TestCase):
 
     def test_invalid_input(self):
-        budgetService = BudgetService()
+        budget_service = BudgetService()
         start = datetime.date(2024, 5, 1)
         end = datetime.date(2024, 4, 1)
-        res = budgetService.totalAmount(start, end)
+        res = budget_service.get_total_amount(start, end)
         self.assertEqual(0, res)  # add assertion here
 
     def test_single_day(self):
-        budgetService = BudgetService()
+        budget_service = BudgetService()
         start = datetime.date(2024, 5, 1)
         end = datetime.date(2024, 5, 1)
-        res = budgetService.totalAmount(start, end)
+        res = budget_service.get_total_amount(start, end)
         self.assertEqual(10, res)
 
     def test_partial_month(self):
-        budgetService = BudgetService()
+        budget_service = BudgetService()
         start = datetime.date(2024, 5, 1)
         end = datetime.date(2024, 5, 10)
-        res = budgetService.totalAmount(start, end)
+        res = budget_service.get_total_amount(start, end)
         self.assertEqual(100, res)
 
     def test_whole_month(self):
-        budgetService = BudgetService()
+        budget_service = BudgetService()
         start = datetime.date(2024, 5, 1)
         end = datetime.date(2024, 5, 31)
-        res = budgetService.totalAmount(start, end)
+        res = budget_service.get_total_amount(start, end)
         self.assertEqual
 
     def test_cross_month(self):
-        budgetService = BudgetService()
+        budget_service = BudgetService()
         start = datetime.date(2024, 5, 31)
         end = datetime.date(2024, 6, 2)
-        res = budgetService.totalAmount(start, end)
+        res = budget_service.get_total_amount(start, end)
         self.assertEqual(30, res)
 
     def test_cross_three_month(self):
-        budgetService = BudgetService()
+        budget_service = BudgetService()
         start = datetime.date(2024, 5, 31)
         end = datetime.date(2024, 7, 2)
-        res = budgetService.totalAmount(start, end)
+        res = budget_service.get_total_amount(start, end)
         self.assertEqual(330, res)
 
     def test_cross_year(self):
-        budgetService = BudgetService()
+        budget_service = BudgetService()
         start = datetime.date(2024, 7, 31)
         end = datetime.date(2025, 8, 31)
-        res = budgetService.totalAmount(start, end)
+        res = budget_service.get_total_amount(start, end)
         self.assertEqual(940, res)
 
 
