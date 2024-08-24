0803a5e11cc0f2ff94ffb371b92d179020502649
gia_liao <gia_liao@trendmicro.com>
2024-08-24 19:34:31 +0800

    introduce variables: daily_amount and overlapping_days

--- budget_service.py
+++ budget_service.py
@@ -37,13 +37,17 @@ class BudgetService:
             budget = next(filter(lambda b: b.year_month == current_date.strftime('%Y%m'), budgets), None)
             if budget is not None:
                 if current_date.strftime('%Y%m') == start.strftime('%Y%m'):
-                    overlapping_amount = get_single_day_amount(start.year, start.month) * (
-                            get_days_in_month(start.year, start.month) - start.day + 1)
+                    daily_amount = get_single_day_amount(start.year, start.month)
+                    overlapping_days = get_days_in_month(start.year, start.month) - start.day + 1
+                    overlapping_amount = daily_amount * overlapping_days
                 elif current_date.strftime('%Y%m') == end.strftime('%Y%m'):
-                    overlapping_amount = get_single_day_amount(end.year, end.month) * end.day
+                    daily_amount = get_single_day_amount(end.year, end.month)
+                    overlapping_days = end.day
+                    overlapping_amount = daily_amount * overlapping_days
                 else:
-                    overlapping_amount = get_single_day_amount(current_date.year, current_date.month) * get_days_in_month(
-                        current_date.year, current_date.month)
+                    daily_amount = get_single_day_amount(current_date.year, current_date.month)
+                    overlapping_days = get_days_in_month(current_date.year, current_date.month)
+                    overlapping_amount = daily_amount * overlapping_days
                 total_amount += overlapping_amount
             current_date += relativedelta(months=1)
 
