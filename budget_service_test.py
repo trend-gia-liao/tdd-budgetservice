770e53051e5133011086fa2e1a9dddd91c1aea1a
gia_liao <gia_liao@trendmicro.com>
2024-08-25 14:37:16 +0800

    extract method: total_amount_should_be

--- budget_service_test.py
+++ budget_service_test.py
@@ -11,44 +11,40 @@ class BudgetServiceTestCase(unittest.TestCase):
     def test_invalid_input(self):
         start = datetime.date(2024, 5, 1)
         end = datetime.date(2024, 4, 1)
-        res = self.budget_service.get_total_amount(start, end)
-        self.assertEqual(0, res)  # add assertion here
+        self.total_amount_should_be(0, start, end)
 
     def test_single_day(self):
         start = datetime.date(2024, 5, 1)
         end = datetime.date(2024, 5, 1)
-        res = self.budget_service.get_total_amount(start, end)
-        self.assertEqual(10, res)
+        self.total_amount_should_be(10, start, end)
 
     def test_partial_month(self):
         start = datetime.date(2024, 5, 1)
         end = datetime.date(2024, 5, 10)
-        res = self.budget_service.get_total_amount(start, end)
-        self.assertEqual(100, res)
+        self.total_amount_should_be(100, start, end)
 
     def test_whole_month(self):
         start = datetime.date(2024, 5, 1)
         end = datetime.date(2024, 5, 31)
-        res = self.budget_service.get_total_amount(start, end)
-        self.assertEqual(310, res)
+        self.total_amount_should_be(310, start, end)
 
     def test_cross_month(self):
         start = datetime.date(2024, 5, 31)
         end = datetime.date(2024, 6, 2)
-        res = self.budget_service.get_total_amount(start, end)
-        self.assertEqual(30, res)
+        self.total_amount_should_be(30, start, end)
 
     def test_cross_three_month(self):
         start = datetime.date(2024, 5, 31)
         end = datetime.date(2024, 7, 2)
-        res = self.budget_service.get_total_amount(start, end)
-        self.assertEqual(330, res)
+        self.total_amount_should_be(330, start, end)
 
     def test_cross_year(self):
         start = datetime.date(2024, 7, 31)
         end = datetime.date(2025, 8, 31)
-        res = self.budget_service.get_total_amount(start, end)
-        self.assertEqual(940, res)
+        self.total_amount_should_be(940, start, end)
+
+    def total_amount_should_be(self, expected, start, end):
+        self.assertEqual(expected, self.budget_service.get_total_amount(start, end))
 
 
 if __name__ == '__main__':
